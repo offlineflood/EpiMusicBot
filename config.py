d2fd8d7bc601c74d1139db61e21f985a544813a1
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-07-02 16:55:56 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os import getenv
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,18 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = "https://graph.org/file/16805c3220aff0f9b74be.jpg"
-START_IMG = "https://graph.org/file/16805c3220aff0f9b74be.jpg"
-
-PING_IMG = getenv("PING_IMG","https://graph.org/file/16805c3220aff0f9b74be.jpg")
-START_IMG = getenv("START_IMG","https://graph.org/file/16805c3220aff0f9b74be.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/justtalkinggrop")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/justtalkinggrop")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6212838686").split()))
 
 
-FAILED = "https://graph.org/file/2793aed7b5c4f7a06beea.jpg"
+FAILED = "https://graph.org/file/52cc2b744f86802aa82ac.jpg"
