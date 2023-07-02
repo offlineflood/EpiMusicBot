ccd635e2351e0d81ff1ad8cfbd9c8dc6f8ae7d89
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-07-02 17:52:17 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
-START_IMG = getenv("START_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/83aa2ccb6d3feabcf5686.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/83aa2ccb6d3feabcf5686.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/justtalkinggrop")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/justtalkinggrop")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/WinstonBotSupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/WinstonResmi")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6212838686").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6252136291").split()))
 
 
-FAILED = "https://graph.org/file/52cc2b744f86802aa82ac.jpg"
+FAILED = "https://graph.org/file/83aa2ccb6d3feabcf5686.jpg"
