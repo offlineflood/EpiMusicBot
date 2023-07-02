56a28531b49fc0bcea6a263f86279eefa1adca01
Aliyev Ali <108521240+offlineflood@users.noreply.github.com>
2023-07-02 15:31:47 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG")
-START_IMG = getenv("START_IMG")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/16805c3220aff0f9b74be.jpg")
 
 SESSION = getenv("SESSION", None)
 
